02bb523302e70b55f13b7bb245d86a51d310a06f
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-11-12 11:11:28 +0200

    created a simple Chore Card and a Chore list

--- app/src/main/java/com/bogdan/choresmap/MainActivity.kt
+++ app/src/main/java/com/bogdan/choresmap/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bogdan.choresmap.ui.theme.ChoresMapTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +39,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Chore(name: String, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = name,
+                modifier = Modifier
+                    .padding(16.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun ChoreList(chores: List<String>) {
+    LazyColumn() {
+        items(chores.size) { index ->
+            Chore(
+                name = chores[index],
+                modifier = Modifier
+                    .padding(
+                        horizontal = 8.dp,
+                        vertical = 2.dp
+                    )
+                )
+        }
+    }
+}
+
+@Composable
+fun ChoresScreen() {
+    val chores = listOf("Buy groceries", "Clean the house", "Walk the dog")
+    ChoreList(chores = chores)
+}
+
+@Preview
+@Composable
+fun PreviewChoresScreen() {
+    ChoresScreen()
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -38,10 +95,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ChoresMapTheme {
         Greeting("Android")
     }
 }
+
