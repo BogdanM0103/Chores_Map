bce42765ce4b34ebc0a0d1f37066e875f39e3a21
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-11-15 16:28:24 +0200

    removed unused imports

--- app/src/main/java/com/bogdan/choresmap/MainActivity.kt
+++ app/src/main/java/com/bogdan/choresmap/MainActivity.kt
@@ -5,30 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.bogdan.choresmap.model.Chore
-import com.bogdan.choresmap.model.ChoreRepository
 import com.bogdan.choresmap.ui.theme.ChoresMapTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import com.bogdan.choresmap.ui.components.ChoresListTest
 
 
@@ -39,10 +18,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChoresMapTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
                     ChoresListTest(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -51,19 +26,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview
-//@Composable
-//fun GreetingPreview() {
-//    ChoresMapTheme {
-//        Greeting("Android")
-//    }
-//}
