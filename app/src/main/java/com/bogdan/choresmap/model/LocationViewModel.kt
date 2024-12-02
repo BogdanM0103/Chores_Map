0da24ae2b2dc1d46d3edad4bed22408ee9ac84ae
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-12-02 15:13:49 +0200

    commented

--- app/src/main/java/com/bogdan/choresmap/model/LocationViewModel.kt
+++ app/src/main/java/com/bogdan/choresmap/model/LocationViewModel.kt
@@ -5,6 +5,10 @@ import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
+/**
+ * To make the location accessible across screens, store it in your LocationViewModel.
+ */
+
 class LocationViewModel : ViewModel() {
     private val _userLocation = MutableStateFlow<LatLng?>(null)
     val userLocation : StateFlow<LatLng?> get() = _userLocation
