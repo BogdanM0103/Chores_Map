fc76d12ba2071fc1c77d6a5495b7f950343d316a
Bogdan Mitru Alexandru <bogdan.mitru@student.upt.ro>
2024-12-03 18:35:13 +0200

    updates

--- app/src/main/java/com/bogdan/choresmap/ui/screens/MapScreen.kt
+++ app/src/main/java/com/bogdan/choresmap/ui/screens/MapScreen.kt
@@ -26,7 +26,6 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
-import com.bogdan.choresmap.ui.components.fetchLocationWithGPS
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
